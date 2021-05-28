70149e65ff7a27ef084f5d5defdbe5c127aa4235
dissidius20 <andayajoshua96@gmail.com>
2021-05-28 18:13:44 +0800

    Initial commit

---
+++ MyLoginForm/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin"
+  }
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+  
+  const Login = details => {
+    console.log(details);
+
+    if ((details.email == adminUser.email && details.password == adminUser.password) && (details.name != "")){
+      console.log("Logged in!");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+    
+      setError("Invalid email or password!");
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""});
+  }
+  return (
+    <div className="App">
+     {(user.email != "") ? (
+       <div className="Welcome">
+       <h2>Welcome, <span>{user.name}</span></h2>
+       <button onClick={Logout}>Logout</button>
+       </div>
+
+     ) : (
+       <LoginForm Login={Login} error={error} />
+     )}
+    </div>
+  );
+}
+
+export default App;
